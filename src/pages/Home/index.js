5d4925b1c71e5e2d8881b3d62d321185a4541b8e
Cristina Jung <cristijung@gmail.com>
2022-11-18 17:49:45 -0300

    algo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,44 @@ import styles from './Home.module.scss';
 import logo from '../../assets/logo-rok.png';
 import Header from 'components/Header';
 
+const categorias = [
+    {
+        nome: 'Heros',
+        thumbnail: herosThumb,
+        header: herosHeader,
+        id: 'heros',
+        descricao: 'Os mais poderosos heróis para você desenvolver'
+    }, 
+    {
+        nome: 'Dragons',
+        thumbnail: dragonsThumb,
+        header: dragonsHeader,
+        id: 'dragons',
+        descricao: 'Os monstros estão prontos para queimar e devorar'   
+    }, 
+    {
+        nome: 'Arqueiros',
+        thumbnail: arqueirosThumb,
+        header: arqueirosHeader,
+        id: 'arqueiros',
+        descricao: 'Desenvolva seu poder militar com arqueiros poderosos!'
+    },
+    {
+        nome: 'Infantaria',
+        thumbnail: infantariaThumb,
+        header: infantariaHeader,
+        id: 'infantaria',
+        descricao: 'Desenvolva seu poder militar com a infantaria que produz mais dano!'
+    },
+    {
+        nome: 'Cavalaria',
+        thumbnail: cavalariaThumb,
+        header: cavalariaHeader,
+        id: 'cavalaria',
+        descricao: 'Desenvolva seu poder militar com a cavalaria!'
+    }
+]
+
 export default function Home() {
     <>
         <Header
@@ -13,3 +51,4 @@ export default function Home() {
         
     </>
 }
+
