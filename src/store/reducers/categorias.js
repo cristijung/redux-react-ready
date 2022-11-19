cf2466dedf2023918fdafdd5ee51a647f4c3ee47
Cristina Jung <cristijung@gmail.com>
2022-11-19 17:13:59 -0300

    ajuste imagens

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,43 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
-import automotivoThumb from 'assets/categorias/thumbnail/automotivo.png';
-import eletronicosThumb from 'assets/categorias/thumbnail/eletronicos.png';
-import escritorioThumb from 'assets/categorias/thumbnail/escritorio.png';
-import jogosThumb from 'assets/categorias/thumbnail/jogos.png';
-import somThumb from 'assets/categorias/thumbnail/som.png';
-import automotivoHeader from 'assets/categorias/header/automotivo.png';
-import eletronicosHeader from 'assets/categorias/header/eletronicos.png';
-import escritorioHeader from 'assets/categorias/header/escritorio.png';
-import jogosHeader from 'assets/categorias/header/jogos.png';
-import somHeader from 'assets/categorias/header/som.png';
+import drgThumb from 'assets/categorias/thumbnail/dragao-mini.png';
+import heroThumb from 'assets/categorias/thumbnail/hero-mini.png';
+import infantariaThumb from 'assets/categorias/thumbnail/inf-mini.png';
+import arqThumb from 'assets/categorias/thumbnail/arq-mini.png';
+import cavalariaThumb from 'assets/categorias/thumbnail/cava-mini.png';
+import drgHeader from 'assets/categorias/header/dragao-capa.png';
+import heroHeader from 'assets/categorias/header/hero-capa.png';
+import infantariaHeader from 'assets/categorias/header/inf-capa.png';
+import arqHeader from 'assets/categorias/header/arq-capa.png';
+import cavalariaHeader from 'assets/categorias/header/cava-capa.png';
 
 const initialState = [{
   nome: 'Heros',
-  thumbnail: eletronicosThumb,
-  header: eletronicosHeader,
+  thumbnail: heroThumb,
+  header: heroHeader,
   id: 'eletronicos',
   descricao: 'Os mais poderosos heróis para você desenvolver'
 }, {
   nome: 'Dragons',
-  thumbnail: automotivoThumb,
-  header: automotivoHeader,
+  thumbnail: drgThumb,
+  header: drgHeader,
   id: 'automotivos',
   descricao: 'Os monstros estão prontos para queimar e devorar'
 }, {
   nome: 'Arqueiros',
-  thumbnail: jogosThumb,
-  header: jogosHeader,
+  thumbnail: arqThumb,
+  header: arqHeader,
   id: 'jogos',
   descricao: 'Desenvolva seu poder militar com arqueiros poderosos!'
 }, {
   nome: 'Infantaria',
-  thumbnail: escritorioThumb,
-  header: escritorioHeader,
+  thumbnail: infantariaThumb,
+  header: infantariaHeader,
   id: 'escritorio',
   descricao: 'Desenvolva seu poder militar com a infantaria que produz mais dano!'
 }, {
   nome: 'Cavalaria',
-  thumbnail: somThumb,
-  header: somHeader,
+  thumbnail: cavalariaThumb,
+  header: cavalariaHeader,
   id: 'som',
   descricao: 'Desenvolva seu poder militar com a cavalaria!'
 }];
